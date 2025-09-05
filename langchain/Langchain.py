629581c9010ba6ff0053440256333fb41f27df05
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-09-05 16:21:56 -0300

    Update Langchain.py

--- langchain/Langchain.py
+++ langchain/Langchain.py
@@ -1,12 +1,17 @@
-# Imports necessários
+import os
+import boto3
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 from langchain.llms.bedrock import Bedrock
+from vector import VectorDB 
 
-from vector import VectorDB
+AWS_REGION = os.getenv("AWS_DEFAULT_REGION", "us-east-1")
 
+bedrock_client = boto3.client(
+    service_name="bedrock-runtime",
+    region_name=AWS_REGION
+)
 
-# TEMPLATE PARA O PROMPT
 QUERY_PROMPT_TEMPLATE = """\
 H:
 Answer the question based on the provided context. Do not create false information.
@@ -15,12 +20,16 @@ Question: {question}
 A:
 """
 
-# Criação da chain
+prompt = PromptTemplate.from_template(QUERY_PROMPT_TEMPLATE)
+
+vector_db = VectorDB()
+retriever = vector_db.as_retriever(search_kwargs={'k': 5})
+
+llm = Bedrock(model_id='anthropic.claude-v2', client=bedrock_client)
+
 qa_chain = RetrievalQA.from_chain_type(
-    llm=Bedrock(model_id='anthropic.claude-v2', client=Bedrock),
-    retriever=VectorDB.as_retriever(search_kwargs={'k': 5}),
+    llm=llm,
+    retriever=retriever,
     return_source_documents=True,
-    chain_type_kwargs={
-        "prompt": PromptTemplate.from_template(QUERY_PROMPT_TEMPLATE)
-    }
+    chain_type_kwargs={"prompt": prompt}
 )
