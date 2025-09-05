ce3c08c076196603433954b1a817fb588c1c3ca0
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-09-05 16:31:13 -0300

    Update BotTelegram.py

--- telegram/BotTelegram.py
+++ telegram/BotTelegram.py
@@ -1,16 +1,16 @@
-import token
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from config import TOKEN
 
-def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-       update.message.reply_text('Hello! I am your bot. How can I help you today?')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('Hello! I am your bot. How can I help you today?')
 
 def main():
-       application = ApplicationBuilder().token(token).build()
+    application = ApplicationBuilder().token(TOKEN).build()
 
-       application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("start", start))
 
-       application.run_polling()
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
